8b2aacf1436f26bf100edfba79e61ba32224254a
justinator525 <jrperez525@gmail.com>
2019-10-22 18:52:12 -0400

    Add AudioManager to Project

---
+++ Assets/_CRYPTID_HUNTER/_Scripts/AudioManager/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+// To access audio clips in other classes simply do: 
+// FindObjectOfType<AudioManaager>().Play("soundYouWantToPlay");
+
+// This will also be a singleton class
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log(name + " auido cannot be found");
+            return;
+        }
+        s.source.Play();
+        Debug.Log("Played");
+    }
+}
