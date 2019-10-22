1ef496500f223bff70dd2da8e942e821965ecee6
Chris McCole <chrismccole1@gmail.com>
2019-10-22 18:28:51 -0400

    Added grass generation script, material, png, shader. We need to replace the png as it was from the internet! Added Terrain Tag so that the grass will generate on the ground only, although, for some reason, when rebaking it, it tends to generate on the player's head, just generate it before putting the player in for now.

---
+++ Assets/_CRYPTID_HUNTER/_Scripts/Grass/GrassGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+[ExecuteInEditMode]
+public class GrassGenerator : MonoBehaviour
+{
+
+    [SerializeField] private MeshFilter meshFilter;
+    [OnValueChanged("GenerateGrass")]
+    [SerializeField] private int seed;
+    [Range(1, 60000)]
+    [SerializeField] private int numberOfGrassTufts;
+    [SerializeField] private Vector2 areaToGrow;
+    [SerializeField] private float startHeight;
+    [SerializeField] private float grassOffset;
+    [SerializeField] private LayerMask hitLayer;
+
+    private Transform cachedTransform;
+    private Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cachedTransform = transform;
+        GenerateGrass();
+    }
+
+    [Button("GenerateGrass")]
+    private void GenerateGrass()
+    {
+        Random.InitState(seed);
+        List<Vector3> positions = new List<Vector3>(numberOfGrassTufts);
+        List<int> indicies = new List<int>(numberOfGrassTufts);
+        List<Color> colors = new List<Color>(numberOfGrassTufts);
+        List<Vector3> normals = new List<Vector3>(numberOfGrassTufts);
+        for (int i = 0; i < numberOfGrassTufts; ++i)
+        {
+            Vector3 origin = this.transform.position;
+            origin.y = startHeight;
+            origin.x += areaToGrow.x * Random.Range(-0.5f, 0.5f);
+            origin.z += areaToGrow.y * Random.Range(-0.5f, 0.5f);
+            Ray ray = new Ray(origin, Vector3.down);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, hitLayer))
+            {
+                Vector3 grassPos = hit.point;
+                grassPos.y += grassOffset;
+                grassPos -= this.transform.position;
+
+                positions.Add(grassPos);
+                indicies.Add(i);
+                colors.Add(new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1));
+                normals.Add(hit.normal);
+            }
+        }
+        mesh = new Mesh();
+        mesh.SetVertices(positions);
+        mesh.SetIndices(indicies.ToArray(), MeshTopology.Points, 0);
+        mesh.SetColors(colors);
+        mesh.SetNormals(normals);
+        meshFilter.mesh = mesh;
+    }
+}
