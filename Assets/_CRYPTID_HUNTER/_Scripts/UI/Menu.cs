131fc0795c22855f887d1612e40bdbfbf98c1567
Jason <jwang11211@gmail.com>
2019-10-23 18:40:14 -0400

    Added win/loss screens

--- Assets/_CRYPTID_HUNTER/_Scripts/UI/Menu.cs
+++ Assets/_CRYPTID_HUNTER/_Scripts/UI/Menu.cs
@@ -1,20 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField, Tooltip ("UI for entire pause menu")]
     GameObject pauseMenu;
-    [SerializeField]
+    [SerializeField, Tooltip("UI for main pause menu with all the buttons")]
     GameObject pauseMain;
-    [SerializeField]
+    [SerializeField, Tooltip("UI for settings menu that shows up after clicking settings")]
     GameObject settings;
+    [SerializeField, Tooltip("UI for screen that shows up when winning")]
+    GameObject winScreen;
+    [SerializeField, Tooltip("UI for screen that shows up when losing")]
+    GameObject loseScreen;
+
+
+    [SerializeField, Tooltip("TMPro Text that displays score")]
+    TextMeshProUGUI scoreDisplay;
+    [SerializeField, Tooltip("TMPro Text that displays the lose condition")]
+    TextMeshProUGUI loseCondition;
+
+    [SerializeField, Tooltip("Score that shows up on win and lose screen")]
+    PhotoScore photoScore;
 
     [SerializeField]
-    PauseManager pauseManager;
+    PhotoCamera photoCamera;
 
+    [SerializeField]
+    RawImage bestPhoto;
+
+    
+
+
+    public void Win()
+    {
+        scoreDisplay.text = $"Score: {photoScore.Score}";
+        winScreen.SetActive(true);
+    }
+
+    public void Lose()
+    {
+        scoreDisplay.text = $"Score: {photoScore.Score}";
+        loseScreen.SetActive(true);
+        if (photoCamera.PhotoCount == photoCamera.MaxPhotos)
+        {
+            loseCondition.text = "You ran out of film.";
+            loseScreen.SetActive(true);
+        }
+    }
 
     public void PlayButton()
     {
@@ -26,6 +63,8 @@ public class Menu : MonoBehaviour
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
         pauseMenu.SetActive(false);
+        winScreen.SetActive(false);
+        loseScreen.SetActive(false);
     }
 
     public void SettingsButton()
@@ -38,6 +77,8 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
         pauseMenu.SetActive(false);
+        winScreen.SetActive(false);
+        loseScreen.SetActive(false);
     }
 
     public void QuitButton()
