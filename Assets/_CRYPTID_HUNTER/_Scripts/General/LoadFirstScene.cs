e5dce27deacbd969369eb2cac6c377be3e888f3e
Chris McCole <chrismccole1@gmail.com>
2019-10-29 16:13:00 -0400

    Sun script for lerping the lighting values. updated lighting settings, moon lighting, ambient lighting and player prefab to make sure they can always see the moon!

--- Assets/_CRYPTID_HUNTER/_Scripts/General/LoadFirstScene.cs
+++ Assets/_CRYPTID_HUNTER/_Scripts/General/LoadFirstScene.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using CodingJar.MultiScene;
 
 public class LoadFirstScene : MonoBehaviour
-{ 
+{
     public void Start()
     {
         SceneManager.LoadScene((int)Level.Menu);
---
+++ Assets/_CRYPTID_HUNTER/_Scripts/Lighting/Sun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class Sun : MonoBehaviour
+{
+    [SerializeField] float maxIntensity = 3f;
+    [SerializeField] float minIntensity = 0f;
+    [SerializeField] AnimationCurve sunIntensityCurve;
+    Light sunLight;
+
+    private Transform cachedTransform;
+
+    private void Start()
+    {
+        sunLight = GetComponent<Light>();
+        cachedTransform = transform;
+    }
+
+    private void Update()
+    {
+        sunLight.intensity = sunIntensityCurve.Evaluate(cachedTransform.rotation.eulerAngles.x / 180f)*-maxIntensity;
+    }
+}
